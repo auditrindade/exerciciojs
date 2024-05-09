19f9daeab655674d658ede4e0e78755b589b9a84
auditrindade <166818804+auditrindade@users.noreply.github.com>
2024-05-09 10:43:39 -0300

    exercicio 3

---
+++ ex3.js
@@ -0,0 +1,20 @@
+//Exercício 3: Ímpar ou par
+//Faça um programa que receba um número do usuário e informe se este número é par ou
+//ímpar.
+
+
+
+//Faça um programa que receba um número do usuário
+
+let numero = parseInt(prompt("Digite um numero"))
+
+
+
+
+//informe se este número é par ou ímpar.
+
+if (numero  %2 == 0 ){
+    console.log ("Esse é um número par")
+    }else {
+     console.log ("Esse é um número ímpar")
+    }
