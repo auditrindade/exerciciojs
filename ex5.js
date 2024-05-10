d8bad8ad9d2ff245c040371f30173644706a036f
auditrindade <166818804+auditrindade@users.noreply.github.com>
2024-05-10 09:26:35 -0300

    feat: Exercicio 5

---
+++ ex5.js
@@ -0,0 +1,28 @@
+//Exercício 5: Verificação de números em ordem crescente
+//Escreva um programa que solicita três números ao usuário e verifica se eles estão em ordem
+//crescente. Exibindo uma mensagem indicando se os números estão em ordem crescente ou
+//não.
+
+
+//Escreva um programa que solicita três números ao usuário
+let numero1 = parseInt(prompt("Digite primeiro numero:"));
+
+let numero2 = parseInt(prompt("Digite segundo numero"));
+
+let numero3 = parseInt(prompt("Digite terceiro numero"));
+
+
+//e verifica se eles estão em ordem crescente.
+
+if (numero1< numero2 < numero3 );
+
+
+
+
+
+
+
+
+//Exibindo uma mensagem indicando se os números estão em ordem crescente ou não.
+
+console.log("Os numeros estão na ordem crescente.")
