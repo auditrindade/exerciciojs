8542ac0e6e729d869207f0860970e652b1ade7af
auditrindade <166818804+auditrindade@users.noreply.github.com>
2024-05-13 10:51:59 -0300

    feat estrutura de repeticao

---
+++ loop/exemplo.js
@@ -0,0 +1,54 @@
+//for - lop contato
+
+//contando do 0 a 10
+// for(let num = 0; num <= 10; num++){
+//     console.log(num);
+// }
+
+// //Contagem regressiva
+// for(let cont = 10; cont >= 0; cont--){
+//     console.log(cont);
+// }
+
+// //while - loop condicional
+
+// let salario = 1000;
+
+// while(salario < 5000){
+//     //salario = salario +100; ou
+//     salario +=100;
+
+//     console.log("O salario ainda e R$" + salario)
+// }
+
+
+// // Do while - loop condicional
+
+// let saldo = 1000;
+
+// do{
+// console.log("Esse e o saldo: + saldo");
+// saldo += 100;
+
+// }while(saldo < 5000);
+
+//array estrutura de dados que armazenamos uma lista dentro de uma variavel/ sempre comeca com 0
+
+let frutas = ["Banana","Laranja","Maça","Pitaya","Melancia"]
+
+console.log (frutas[3])
+
+// //forEach = percorre uma lista
+// frutas.forEach(fruta => {
+//     console.log("A fruta e: " + fruta);
+// });
+
+//percorre uma lista com for
+// .length = quantidade de um elemento dentro de um array
+for(let indice = 0; indice < 5; indice ++){
+    console.log(frutas[indice]);
+}
+
+
+
+
